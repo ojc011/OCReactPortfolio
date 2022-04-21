8e963ae24c57ea7a6c024b9746587e54f8b65c59
Oliver C <ocronk10@gmail.com>
2022-04-21 17:32:49 -0500

    Minor css changes

--- projects/ocreactportfolio/src/views/Resume/index.js
+++ projects/ocreactportfolio/src/views/Resume/index.js
@@ -3,16 +3,19 @@ import "./style.css";
 import ResumePDF from "../../components/ResumePDF/OliverCPortfolio.pdf";
 
 function Resume() {
-
-    return (
-      <div className="viewFour" id="resume">
-        <div className="row">
-          <div className="col-md">
-            <a href={ResumePDF} target="_blank"><h3 className="viewTitle" id="resumeTitle">RESUME</h3></a>
-          </div>
+  return (
+    <div className="viewFour" id="resume">
+      <div className="row">
+        <div className="col-md">
+          <a href={ResumePDF} target="_blank" className="resLink">
+            <h3 className="viewTitle" id="resumeTitle">
+              RESUME
+            </h3>
+          </a>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Resume;
