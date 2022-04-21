7180b3ad0f7e39535a2cb4cf52dfd202430e6b1e
Oliver C <ocronk10@gmail.com>
2022-04-21 17:22:33 -0500

    updated syling and comments for a better look

--- projects/ocreactportfolio/src/views/About/index.js
+++ projects/ocreactportfolio/src/views/About/index.js
@@ -3,38 +3,53 @@ import MeOnWater from "../../img/ocean.jpg";
 import "./style.css";
 
 function About() {
-  return (
-    <div className="viewThree" id="about">
-      <div className="row">
-        <div className="col-md about-left">
-          <img src={MeOnWater} className="about-photo img-fluid" alt="Me"></img>
-        </div>
-        <div className="col-md">
-          <h3 className="viewTitle" id="aboutTitle">
-            ABOUT
-          </h3>
-          <p className="bio">
-            I'm an early-career software engineer with a background in front end development as well as back end. I am looking forward to dive into the industry and contribute as much as I can!
-          </p>
-          <p className="bio">
-            Some of the skills I bring to the table:
-            <ul>
-              <li>Adobe Creative Suite (special love for Illustrator)</li>
-              <li>JavaScript</li>
-              <li>ReactJS</li>
-              <li>HTML & CSS</li>
-              <li>Node</li>
-              <li>Express</li>
-              <li>SQL</li>
-              <li>Logo & Web Design</li>
-              <li>Project Management</li>
-            </ul>
-          </p>
-          <p>I look forward to working with you!</p>
+
+    return (
+      <div className="viewThree" id="about">
+        <div className="row">
+          <div className="col-md about-left">
+            <img
+              src={MeOnWater}
+              className="about-photo img-fluid"
+              alt="Me"
+            ></img>
+          </div>
+          <div className="col-md">
+            <h3 className="viewTitle" id="aboutTitle">
+              ABOUT
+            </h3>
+            <div className="row align-self-center bioText">
+              <p className="bio">
+                I'm an early-career software engineer with a background in front
+                end development as well as back end. I am looking forward to
+                dive into the industry and contribute as much as I can!
+              </p>
+              <p className="bio">
+                <span className="skillHeader">
+                  Some of the skills I bring to the table:
+                </span>
+                <ul className=" skillList text-center">
+                  <li className="skill">
+                    Adobe Creative Suite (special love for Illustrator)
+                  </li>
+                  <li className="skill">JavaScript</li>
+                  <li className="skill">ReactJS</li>
+                  <li className="skill">HTML & CSS</li>
+                  <li className="skill">Node</li>
+                  <li className="skill">Express</li>
+                  <li className="skill">SQL</li>
+                  <li className="skill">Logo & Web Design</li>
+                  <li className="skill">Project Management</li>
+                </ul>
+              </p>
+              <p className="bio">I look forward to working with you!</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default About;
+
+
--- projects/ocreactportfolio/src/views/Resume/index.js
+++ projects/ocreactportfolio/src/views/Resume/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./style.css";
+import ResumePDF from "../../components/ResumePDF/OliverCPortfolio.pdf";
 
 function Resume() {
-  return (
-    <div className="viewFour" id="resume">
-      <div className="row">
-        <div className="col-md">
-          <h3 className="viewTitle" id="resumeTitle">
-            RESUME
-          </h3>
+
+    return (
+      <div className="viewFour" id="resume">
+        <div className="row">
+          <div className="col-md">
+            <a href={ResumePDF} target="_blank"><h3 className="viewTitle" id="resumeTitle">RESUME</h3></a>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default Resume;
